8853c4e654e2012fde765414f05f4376108d8188
bosciu <bosciu@icloud.com>
2021-05-06 15:04:10 +0200

    Completamento esercizio

--- js/script.js
+++ js/script.js
@@ -8,14 +8,48 @@ Consigli del giorno:
 * Individuate gli elementi di cui avete bisogno per realizzare il programma. */
 
 /* FUNCTIONS */
+/**
+ * 
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns Number with extrem both
+ */
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * 
+ * @param {*} element 
+ * @param {Array} array
+ * @returns true if the element is in array 
+ */
+function isInArray(element, array) {
+    for (var i = 0; i < array.length; i++) {
+        if (element == array[i]) {
+            return true;
+        }
+    }
+}
+
 /* FUNCTIONS */
 var numeriCasuali = [];
+var numeriGiusti = [];
+var score = 0;
 
-/* Un alert() espone 5 numeri generati casualmente. da 1 a 100*/
+/* Un alert espone 5 numeri generati casualmente. da 1 a 100*/
 for (var i = 0; i < 5; i++) {
     numeriCasuali.push(getRandomNumber(1, 100));
 }
 alert("NUMERI CASUALI: " + numeriCasuali);
+
+setTimeout(function() {
+    for (var i = 0; i < 5; i++) {
+        var numeroInserito = parseInt(prompt("Inserisci un numero di Simon e premi invio!"));
+        if (isInArray(numeroInserito, numeriCasuali)) {
+            numeriGiusti.push(numeroInserito);
+            score++
+        }
+    }
+    alert("I numeri casuali erano: " + numeriCasuali + "\nI numeri che ti sei ricordato sono: " + numeriGiusti + "\nHai totalizzato: " + score + " punti.");
+}, 30000)
